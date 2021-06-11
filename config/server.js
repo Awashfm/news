ab50ab84e7ca86d5de782cb0432887c7fc17c191
miami12 <awasshfm.90.7@gmail.com>
2021-06-11 22:49:10 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c717fe144c683b2601b9400033fd2467'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fc072ce-27bc-47ba-9a36-fd467ea0022f'
+};
